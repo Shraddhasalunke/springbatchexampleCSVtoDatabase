beafb8605c838a2e0b1a42394bfbaa8d03166edf
Shraddha Salunke <91833741+Shraddhasalunke@users.noreply.github.com>
2024-04-12 16:01:53 +0530

    Add files via upload

---
+++ src/test/java/com/example/springbatchexample1/SpringBatchExample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbatchexample1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchExample1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
